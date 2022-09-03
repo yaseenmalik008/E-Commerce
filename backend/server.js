2f6b5974169b864cb21b6ab03f30cef7334c690a
Maliky853 <maliky853@gmail.com>
2022-09-04 05:04:42 +0530

    step 13 completed connected to database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import  express  from 'express';
 import products from './data/products.js';
 import dotenv from "dotenv"
-
+import connectDB from "./config/db.js"
 dotenv.config();
+connectDB()
 
 const app = express();
 
@@ -22,5 +23,5 @@ app.get('/api/products/:id', (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(process.env.PORT, () => {
-  console.log('Server running on port 5000');
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
