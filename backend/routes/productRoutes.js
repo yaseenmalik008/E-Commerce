524476f60e2475a9e9229a97c91daf2e43114a0b
Maliky853 <maliky853@gmail.com>
2022-09-05 14:29:30 +0530

    Step 18 completed. Fetching products from the database done.

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,34 @@
+import express from "express";
+import asyncHandler from "express-async-handler";
+import Product from "../models/productModel.js"
+
+const router = express.Router()
+
+router.get(
+    '/',
+    asyncHandler(async(req,res)=>{
+const products = await Product.find({});
+res.json(products);
+    })
+);
+
+router.get(
+    "/:id",
+    asyncHandler(async(req,res)=>{
+        try {
+            const product =await Product.findById(req.params.id);
+
+if(product){
+    res.json(product)
+}else{
+    res.status(404).json({message : "Product Not Found"})
+}
+
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({message : "Something Went Wrong"})
+        }
+    })
+)
+
+export default router
