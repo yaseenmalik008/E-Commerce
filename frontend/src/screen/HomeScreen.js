794c82735a8c8074d3036d9e8bf401366e9dc4ac
Maliky853 <maliky853@gmail.com>
2022-09-08 04:54:37 +0530

    Step 23 completed. Getting Redux State in the Home Screen done.

--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -1,39 +1,43 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Heading, Grid } from '@chakra-ui/react';
-import Product from '../Components/Product';
-
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Heading, Grid } from "@chakra-ui/react";
+import Product from "../Components/Product";
+import { listProducts } from "../actions/productActions";
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
-      setProducts(data);
-    };
-
-    fetchProducts();
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <div>
-      <Heading as='h2' mb='8' fontSize='3xl'>
+      <Heading as="h2" mb="8" fontSize="3xl">
         Latest Products
       </Heading>
 
-      <Grid
-        templateColumns={{
-          sm: '1fr',
-          md: '1fr 1fr',
-          lg: '1fr 1fr 1fr',
-          xl: '1fr 1fr 1fr 1fr',
-        }}
-        gap='8'
-      >
-        {products.map((prod) => (
-          <Product key={prod._id} product={prod} />
-        ))}
-      </Grid>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+        <Grid
+          templateColumns={{
+            sm: "1fr",
+            md: "1fr 1fr",
+            lg: "1fr 1fr 1fr",
+            xl: "1fr 1fr 1fr 1fr",
+          }}
+          gap="8"
+        >
+          {products.map((prod) => (
+            <Product key={prod._id} product={prod} />
+          ))}
+        </Grid>
+      )}
     </div>
   );
 };
