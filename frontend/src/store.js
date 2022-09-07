ef14b2460ae73676f327ee9b343610e4c9d2a3c8
Maliky853 <maliky853@gmail.com>
2022-09-08 04:43:49 +0530

    Step 22 completed. Create our first reducer done.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,10 @@
 import {applyMiddleware,combineReducers,createStore} from 'redux'
 import {composeWithDevTools} from "redux-devtools-extension"
 import thunk from 'redux-thunk'
-
-const reducer = combineReducers({})
+import { productListReducer } from './reducers/productReducer'
+const reducer = combineReducers({
+    productList : productListReducer,
+})
 
 const initialState = {}
 
