e9d766522ec2227c820bfa1af0f2f216926289b9
Maliky853 <maliky853@gmail.com>
2022-09-12 13:55:27 +0530

    Step 28 completed. Cart Functionality done.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,12 +1,14 @@
 import {applyMiddleware,combineReducers,createStore} from 'redux'
 import {composeWithDevTools} from "redux-devtools-extension"
 import thunk from 'redux-thunk'
+import { cartReducer } from './reducers/cartReducer'
 import { productListReducer,productDetailsReducer } from './reducers/productReducer'
 
 
 const reducer = combineReducers({
     productList : productListReducer,
     productDetails : productDetailsReducer,
+    cart : cartReducer,
 })
 
 const initialState = {}
