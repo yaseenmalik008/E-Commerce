9980a59ef6a1f9f56790e0f8853c60b049be1902
Maliky853 <maliky853@gmail.com>
2022-09-16 09:29:06 +0530

    fix bugs

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,34 +1,30 @@
-import asyncHandler from 'express-async-handler'
+import asyncHandler from 'express-async-handler';
 import Product from '../models/productModel.js';
 
-// @desc fetch all products
-// @routes  GET /api/products
+// @desc    Fetch all products
+// @route   GET /api/products
 // @access  public
+const getProducts = asyncHandler(async (req, res) => {
+	const products = await Product.find({});
+	res.json(products);
+});
 
-const getProducts = asyncHandler(async(req,res)=>{
-    const products = await Product.find({});
-    res.json(products);
-        })
-
-
-// @desc fetch single product
-// @routes  GET /api/products/:id
+// @desc    Fetch single product
+// @route   GET /api/products/:id
 // @access  public
+const getProductById = asyncHandler(async (req, res) => {
+	try {
+		const product = await Product.findById(req.params.id);
 
-      const getProductById =   asyncHandler(async(req,res)=>{
-            try {
-                const product =await Product.findById(req.params.id);
-    
-    if(product){
-        res.json(product)
-    }else{
-        res.status(404).json({message : "Product Not Found"})
-    }
-    
-            } catch (error) {
-                console.error(error);
-                res.status(500).json({message : "Something Went Wrong"})
-            }
-        })
+		if (product) {
+			res.json(product);
+		} else {
+			res.status(404).json({ message: 'Product not found' });
+		}
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: 'Something went wrong' });
+	}
+});
 
-        export {getProducts,getProductById}
+export { getProductById, getProducts };
--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,24 +1,23 @@
-import asyncHandler from "express-async-handler";
-import User from '../models/userModel.js'
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
 
+const authUser = asyncHandler(async (req, res) => {
+	const { email, password } = req.body;
 
-const authUser = asyncHandler(async(req,res)=>{
-    const {email,password} = req.body
+	const user = await User.findOne({ email });
 
-    const user = await User.findOne({email})
+	if (user && (await user.matchPassword(password))) {
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+			token: null,
+		});
+	} else {
+		res.status(401); // Unauthorized
+		throw new Error('Invalid email or password');
+	}
+});
 
-    if(email && (await user.matchPassword(password))){
-        res.json({
-            _id : user._id,
-            name : user.name,
-            email : user.email,
-            isAdmin : user.isAdmin,
-            token : null,
-        })
-    }else{
-        res.json(401) // Unauthorized
-        throw new Error('Inavalid Email or Password')
-    }
-})
-
-export {authUser}
+export { authUser };
