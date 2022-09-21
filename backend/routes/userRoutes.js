93b7b08b43c40980fb60642ac2e5b9c2d79764d0
Maliky853 <maliky853@gmail.com>
2022-09-21 14:10:14 +0530

    Step 40 completed. Update User Profile endpoint in the backend done.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,11 @@
 import  express from "express";
-import { authUser ,getuserProfile,registerUser} from "../controller/userController.js";
+import { authUser ,getuserProfile,registerUser,updateUserProfile} from "../controller/userController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router()
 
 router.route('/').post(registerUser)
 router.post('/login',authUser)
-router.route('/profile').get(protect,getuserProfile)
+router.route('/profile').get(protect,getuserProfile).put(protect,updateUserProfile)
 
 export default router
