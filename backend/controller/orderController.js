81d5b3304eedbaffea05ecd83ece34bf94558ee1
Maliky853 <maliky853@gmail.com>
2022-09-28 01:45:05 +0530

    Step 52 completed. Backend endpoint for updating an order to paid done.

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -51,4 +51,27 @@ const  getOrderById = asyncHandler(async(req,res)=>{
 	}
 })
 
-export { addOrderItems,getOrderById };
+// @desc    Update Order To Paid
+// @route   PUT /api/orders/:id/pay
+// @access  private
+
+const updateOrderToPaid = asyncHandler(async(req,res)=>{
+	const order = await Order.findById(req.params.id)
+
+	if(order){
+		order.isPaid = true,
+		order.paidAt = Date.now()
+		order.paymentResult  = {
+			id : req.body.id,
+			state : req.body.state,
+			update_time : req.body.update_time,
+			email_address : req.body.email_address,
+		}
+		const updatedOrder = await order.save()
+	}else{
+		res.status(404)
+		throw new Error('Order Not Found')
+	}
+})
+
+export { addOrderItems,getOrderById ,updateOrderToPaid};
