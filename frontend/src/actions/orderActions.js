cc9a8d02731a791302ce9e10e2978b347d9a8cda
Maliky853 <maliky853@gmail.com>
2022-09-27 19:57:30 +0530

    Step 50 completed. Create the order details reducer and action (frontend).

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,35 +1,70 @@
-import axios from 'axios';
+import axios from "axios";
 import {
-	ORDER_CREATE_FAIL,
-	ORDER_CREATE_REQUEST,
-	ORDER_CREATE_SUCCESS,
-} from '../constants/orderConstants';
+  ORDER_CREATE_FAIL,
+  ORDER_CREATE_REQUEST,
+  ORDER_CREATE_SUCCESS,
+  ORDER_DETAILS_SUCCESS,
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_FAIL,
+} from "../constants/orderConstants";
 
 export const createOrder = (order) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: ORDER_CREATE_REQUEST });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+        "Content-Type": "application/json",
+      },
+    };
+
+    const { data } = await axios.post(`/api/orders`, order, config);
+
+    dispatch({ type: ORDER_CREATE_SUCCESS, payload: data });
+  } catch (err) {
+    dispatch({
+      type: ORDER_CREATE_FAIL,
+      payload:
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message,
+    });
+  }
+};
+
+
+export const getOrderDetails = (id) => async(dispatch,getState)=>{
 	try {
-		dispatch({ type: ORDER_CREATE_REQUEST });
+		dispatch ({type : ORDER_DETAILS_REQUEST})
 
 		const {
-			userLogin: { userInfo },
-		} = getState();
+			userLogin : {userInfo}
+		} = getState()
 
 		const config = {
-			headers: {
-				Authorization: `Bearer ${userInfo.token}`,
-				'Content-Type': 'application/json',
-			},
-		};
+			headers : {
+Authorization : `Bearer ${userInfo.token}`
+			}
+		}
 
-		const { data } = await axios.post(`/api/orders`, order, config);
+		const {data} = await axios.get(`/api/orders/${id}` , config)
 
-		dispatch({ type: ORDER_CREATE_SUCCESS, payload: data });
+		dispatch ({type : ORDER_CREATE_SUCCESS , payload : data})
+		
 	} catch (err) {
 		dispatch({
-			type: ORDER_CREATE_FAIL,
-			payload:
-				err.response && err.response.data.message
-					? err.response.data.message
-					: err.message,
-		});
+			type : ORDER_DETAILS_FAIL ,
+			payload : 
+			err.response && err.response.data.message
+			? err.response.data.message
+			:err.message
+		})
+		
 	}
-};
+
+}
