a81d52240df7fc0c62bda90b6886d3b4d76cb44c
Maliky853 <maliky853@gmail.com>
2022-10-02 03:23:32 +0530

    step 62 completed  User Edit screen and User Details screen components

---
+++ frontend/src/screen/UserEditScreen.js
@@ -0,0 +1,127 @@
+import {
+  Button,
+  Checkbox,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Link,
+  Spacer,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link as RouterLink, useNavigate, useParams } from "react-router-dom";
+import { getUserDetails, updateUser } from "../actions/userAction";
+import FormContainer from "../Components/FormContainer";
+import Loader from "../Components/Loader";
+import Message from "../Components/Message";
+import { USER_UPDATE_RESET } from "../constants/userConstants";
+
+const UserEditScreen = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { id: userId } = useParams;
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  const userDetails = useSelector((state) => state.userDetails);
+  const { loading, error, user } = userDetails;
+
+  const userUpdate = useSelector((state) => state.userUpdate);
+  const {
+    loading: loadingUpdate,
+    error: errorUpdate,
+    success: successUpdate,
+  } = userUpdate;
+
+  useEffect(() => {
+    if (successUpdate) {
+      dispatch({ type: USER_UPDATE_RESET });
+      navigate("/admin/userList");
+    } else {
+      if (!user.name || user.id !== userId) {
+        dispatch(getUserDetails(userId));
+      } else {
+        setName(user.name);
+        setEmail(user.email);
+        setIsAdmin(user.isAdmin);
+      }
+    }
+  }, [user, dispatch, navigate, userId, successUpdate]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(updateUser({ _id: userId, email, name, isAdmin }));
+  };
+
+  return (
+    <>
+      <Link as={RouterLink} to="/admin/userList">
+        Go Back
+      </Link>
+      <Flex w="full" alignItems="center" justifyContent="center" py="5">
+        <FormContainer>
+          <Heading as="h1" mb="8" fontSize="3xl">
+            Edit User
+          </Heading>
+        </FormContainer>
+        {loadingUpdate && <Loader />}
+        {errorUpdate && <Message type="error">{errorUpdate}</Message>}
+
+        {laoding ? (
+          <Loader />
+        ) : error ? (
+          <Message type="error">{error}</Message>
+        ) : (
+          <from onSubmit={submitHandler}>
+            <FormControl id="name" isRequired>
+              <FormLabel>Name</FormLabel>
+              <Input
+                type="text"
+                placeholder="Enter Full Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </FormControl>
+
+            <Spacer h="3" />
+
+            <FormControl id="email" isRequired>
+              <FormLabel>Email Address</FormLabel>
+              <Input
+                type="email"
+                placeholder="Enter Email Address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </FormControl>
+
+            <Spacer h="3" />
+
+            <FormControl id="isAdmin" isRequired>
+              <FormLabel>Is Admin?</FormLabel>
+              <Checkbox
+                size="lg"
+                colorScheme="teal"
+                checked={isAdmin}
+                onChange={(e) => setIsAdmin(e.target.value)}
+              >
+                Is Admin?
+              </Checkbox>
+            </FormControl>
+
+            <Spacer h="3" />
+
+            <Button type="submit" isLoading={loading} colorScheme="teal" mt="4">
+              Update
+            </Button>
+          </from>
+        )}
+      </Flex>
+    </>
+  );
+};
+export default UserEditScreen;
